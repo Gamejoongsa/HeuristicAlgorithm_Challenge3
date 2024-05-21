c48c33e99b73115217b36e7a6641a9113f245371
Gamejoongsa <jane10000@naver.com>
2024-05-21 10:02:13 +0900

    ver 1.1 further update

--- agents/_skeleton.py
+++ agents/_skeleton.py
@@ -1,6 +1,7 @@
 from random import choice
 from time import time
-from typing import Tuple, List
+from traceback import print_exc, format_exc
+from typing import Tuple, List, Callable, Dict, Generator
 
 from action import *
 from board import GameBoard
@@ -8,15 +9,21 @@ from board import GameBoard
 
 class Agent:  # Do not change the name of this class!
     """
-    An agent class (Skeleton)
+    An agent class
+
+    The list of algorithms that you can use
+    - AND-OR Search or other variants
+    - Online DFS or other Online variant of uninformed/heuristic search algorithms
+    - LRTA*
     """
 
-    def decide_new_village(self, board: GameBoard, time_limit: float = None) -> Tuple[int, int]:
+    def decide_new_village(self, board: GameBoard, time_limit: float = None) -> Callable[[dict], Tuple[Action, Action]]:
         """
         This algorithm search for the best place of placing a new village.
 
         :param board: Game board to manipulate
         :param time_limit: Timestamp for the deadline of this search.
-        :return: Tuple of coordinates
+        :return: A Program (Function) to execute
         """
-        raise NotImplementedError('Please implement this method!')
+
+        raise NotImplementedError()
